af017ed972df48d9137b1c0f9f7e28d047115e14
Blessedo <blessedvictorigwe@gmail.com>
2025-03-14 17:21:02 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,200 @@
+// Change nav background on scroll
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 0) {
+      navbar.style.backgroundImage = "url('images/dark_bg.png'"; 
+     }
+      else {
+      navbar.style.backgroundImage = "none"; 
+       
+    }
+  });
+  
+    // Number Increment
+  document.addEventListener('DOMContentLoaded', function() {
+    const counters = document.getElementsByClassName('counter');
+    const duration = 12000; // Duration of the animation in milliseconds
+
+    function updateCounter(counter, start, end) {
+        let count = start;
+        const increment = (end - start) / (duration / 100); // Calculate the increment value
+
+        function animate() {
+            count += increment;
+            if (count < end) {
+                counter.textContent = Math.floor(count) + '+';
+                requestAnimationFrame(animate);
+            } else {
+                counter.textContent = end + '+';
+            }
+        }
+        animate();
+    }
+    // Viewport visibility for the increment
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const start = parseInt(entry.target.getAttribute('data-start'));
+                const end = parseInt(entry.target.getAttribute('data-end'));
+                updateCounter(entry.target, start, end);
+                observer.unobserve(entry.target); // Stop observing after the animation starts
+            }
+        });
+    });
+
+    Array.from(counters).forEach(counter => {
+        observer.observe(counter);
+    });
+});
+
+
+
+function menu1(){
+  var menu1= document.getElementById('menu1');
+  var menu2= document.getElementById('menu2');
+  var menu3= document.getElementById('menu3');
+  var menu4= document.getElementById('menu4');
+  var img1 = document.getElementById('menu-image1');
+  var img2 = document.getElementById('menu-image2');
+  var img3 = document.getElementById('menu-image3');
+  var img4 = document.getElementById('menu-image4');
+  var img5 = document.getElementById('menu-image5');
+  var img6 = document.getElementById('menu-image6');
+  menu1.style.color = "#c42900";
+  menu1.style.textDecoration = "underline";
+  menu2.style.color = "black";
+  menu2.style.textDecoration = "none";
+  menu3.style.color = "black";
+  menu3.style.textDecoration = "none";
+  menu4.style.color = "black";
+  menu4.style.textDecoration = "none";
+  img1.src = "images/sl1.png"; 
+  img2.src = "images/sl2.png";
+  img3.src = "images/sl3.png";
+  img4.src = "images/sl4.png";
+  img5.src = "images/sl5.png";
+  img6.src = "images/dish_06.png";
+}
+
+function menu2(){
+  var menu1= document.getElementById('menu1');
+  var menu2= document.getElementById('menu2');
+  var menu3= document.getElementById('menu3');
+  var menu4= document.getElementById('menu4');
+  var img1 = document.getElementById('menu-image1');
+  var img2 = document.getElementById('menu-image2');
+  var img3 = document.getElementById('menu-image3');
+  var img4 = document.getElementById('menu-image4');
+  var img5 = document.getElementById('menu-image5');
+  var img6 = document.getElementById('menu-image6');
+  menu2.style.color = "#c42900";
+  menu2.style.textDecoration = "underline";
+  menu1.style.color = "black";
+  menu1.style.textDecoration = "none";
+  menu3.style.color = "black";
+  menu3.style.textDecoration = "none";
+  menu4.style.color = "black";
+  menu4.style.textDecoration = "none";
+  img1.src = "images/dish_01.png"; 
+  img2.src = "images/dish_02.png";
+  img3.src = "images/dish_03.png";
+  img4.src = "images/dish_04.png";
+  img5.src = "images/dish_05.png";
+  img6.src = "images/dish_06.png";
+}
+
+function menu3(){
+  var menu1= document.getElementById('menu1');
+  var menu2= document.getElementById('menu2');
+  var menu3= document.getElementById('menu3');
+  var menu4= document.getElementById('menu4');
+  var img1 = document.getElementById('menu-image1');
+  var img2 = document.getElementById('menu-image2');
+  var img3 = document.getElementById('menu-image3');
+  var img4 = document.getElementById('menu-image4');
+  var img5 = document.getElementById('menu-image5');
+  var img6 = document.getElementById('menu-image6');
+  menu3.style.color = "#c42900";
+  menu3.style.textDecoration = "underline";
+  menu1.style.color = "black";
+  menu1.style.textDecoration = "none";
+  menu2.style.color = "black";
+  menu2.style.textDecoration = "none";
+  menu4.style.color = "black";
+  menu4.style.textDecoration = "none";
+  img1.src = "images/dst1.png"; 
+  img2.src = "images/sd1.png";
+  img3.src = "images/sd2.png";
+  img4.src = "images/sd3.png";
+  img5.src = "images/sd4.png";
+  img6.src = "images/sd6.png";
+}
+
+function menu4(){
+  var menu1= document.getElementById('menu1');
+  var menu2= document.getElementById('menu2');
+  var menu3= document.getElementById('menu3');
+  var menu4= document.getElementById('menu4');
+  var img1 = document.getElementById('menu-image1');
+  var img2 = document.getElementById('menu-image2');
+  var img3 = document.getElementById('menu-image3');
+  var img4 = document.getElementById('menu-image4');
+  var img5 = document.getElementById('menu-image5');
+  var img6 = document.getElementById('menu-image6');
+  menu4.style.color = "#c42900";
+  menu4.style.textDecoration = "underline";
+  menu1.style.color = "black";
+  menu1.style.textDecoration = "none";
+  menu2.style.color = "black";
+  menu2.style.textDecoration = "none";
+  menu3.style.color = "black";
+  menu3.style.textDecoration = "none";
+  img1.src = "images/dst1.png"; 
+  img2.src = "images/dst2.png";
+  img3.src = "images/dst3.png";
+  img4.src = "images/dst4.png";
+  img5.src = "images/dst5.png";
+  img6.src = "images/dst6.png";
+}
+
+// var hover1 = document.getElementById('menu1');
+// var hover2 = document.getElementById('menu2');
+// var hover3 = document.getElementById('menu3');
+// var hover4 = document.getElementById('menu4');
+
+//     hover1.addEventListener('mouseover', function() {
+//       hover1.style.color = '#c42900';
+//       hover1.style.textDecoration = 'underline';
+//     });
+//     hover2.addEventListener('mouseover', function() {
+//       hover2.style.color = '#c42900';
+//       hover2.style.textDecoration = 'underline';
+//     });  
+//     hover3.addEventListener('mouseover', function() {
+//       hover3.style.color = '#c42900';
+//       hover3.style.textDecoration = 'underline';
+//     });
+//     hover4.addEventListener('mouseover', function() {
+//       hover4.style.color = '#c42900';
+//       hover4.style.textDecoration = 'underline';
+//     });
+
+//     hover1.addEventListener('mouseout', function() {
+//       hover1.style.color = 'black';
+//       hover1.style.textDecoration = 'none';
+//     });
+//     hover2.addEventListener('mouseout', function() {
+//       hover2.style.color = 'black';
+//       hover2.style.textDecoration = 'none';
+//     });  
+//     hover3.addEventListener('mouseout', function() {
+//       hover3.style.color = 'black';
+//       hover3.style.textDecoration = 'none';
+//     });
+//     hover4.addEventListener('mouseout', function() {
+//       hover4.style.color = 'black';
+//       hover4.style.textDecoration = 'none';
+//     });
+  window.onloadstart=menu1();
